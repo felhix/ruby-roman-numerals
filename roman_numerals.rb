587f82c066d5879cedf349eec40e8679cdf445ec
felhix <felix.gaude@gmail.com>
2018-01-17 20:33:10 +0100

    tests des dizaines effectués

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,8 +1,24 @@
 class Fixnum
 
   def to_roman
-	  units_romans = ["I", "II", "III", 'IV', 'V', 'VI', 'VII', "VIII", "IX"]
-	  return units_romans[self - 1]
+    number_in_string = self.to_s
+	  return  to_tenths(number_in_string[-2]) + to_units(number_in_string[-1])
   end
+
+  private
+
+  def to_tenths(tenths)
+    tenths_romans = ["X", "XX", "XXX", 'XL', 'L', 'LX', 'LXX', "LXXX", "XC"]
+    return "" if tenths == "0" || tenths == nil
+    return tenths_romans[tenths.to_i - 1]
+  end
+
+  def to_units(units)
+    units_romans = ["I", "II", "III", 'IV', 'V', 'VI', 'VII', "VIII", "IX"]
+    return "" if units == "0" || units == nil
+    return units_romans[units.to_i - 1]
+  end
+
+
 
 end
--- roman_numerals_test.rb
+++ roman_numerals_test.rb
@@ -44,22 +44,18 @@ class RomanNumeralsTest < Minitest::Test
   end
 
   def test_59
-    skip
     assert_equal 'LIX', 59.to_roman
   end
 
   def test_93
-    skip
     assert_equal 'XCIII', 93.to_roman
   end
 
   def test_141
-    skip
     assert_equal 'CXLI', 141.to_roman
   end
 
   def test_163
-    skip
     assert_equal 'CLXIII', 163.to_roman
   end
 
